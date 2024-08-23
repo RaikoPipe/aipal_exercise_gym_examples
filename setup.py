fd214e14018c64113d180330fcac1f116bff36ec
RaikoPipe <eclipsedust@gmail.com>
2024-08-23 12:55:25 +0200

    - updated pygame version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="gym_examples",
     version="0.0.2",
-    install_requires=["gymnasium==0.29.1", "pygame==2.1.0"],
+    install_requires=["gymnasium==0.29.1", "pygame==2.6.0"],
 )
