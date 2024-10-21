ebcbf7681d95e5468373a44c2140a3b733134470
RaikoPipe <eclipsedust@gmail.com>
2024-10-21 10:05:22 +0200

    - bug fix

--- gym_examples/__init__.py
+++ gym_examples/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
      id="gym_examples/GridWorld-v0",
      entry_point="gym_examples.envs:GridWorldEnv",
-     max_episode_steps=300,
+     max_episode_steps=100,
 )
